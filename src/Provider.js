9d2665db08cdb251151d635321bd760ed5d89243
Alejandro Lariccia <alejandro.lariccia@fizzmod.com>
2021-03-26 19:59:01 -0300

    add Provider docs and test

--- src/Provider.js
+++ src/Provider.js
@@ -2,6 +2,19 @@ import React from 'react';
 import useOauth from './useOauth';
 import {AuthContext} from './context';
 
+/**
+ * @name AuthProvider
+ * @description Provide the app that envolves with OAuth context
+ * @public
+ * @param {object} props - component props
+ * @example const myApp() {
+ *    render (
+ *      <AuthProvider>
+ *        <MyHome />
+ *      <AuthProvider>
+ *    )
+ * }
+ */
 const AuthProvider = (props) => {
   const oauth = useOauth();
 
--- test/Provider.test.js
+++ test/Provider.test.js
@@ -1,14 +1,15 @@
-/**
- * @format
- */
-
 import 'react-native';
 import React from 'react';
 import renderer from 'react-test-renderer';
 import AuthProvider from '../src/Provider';
+import useOauth from '../src/useOauth';
 
-// Note: test renderer must be required after react-native.
+jest.mock('../src/useOauth', () => jest.fn());
 
-it('renders correctly', () => {
-  renderer.create(<AuthProvider />);
+describe('AuthProvider', () => {
+  it('renders correctly and calls useOauth hook', () => {
+    renderer.create(<AuthProvider />);
+
+    expect(useOauth).toBeCalled();
+  });
 });
