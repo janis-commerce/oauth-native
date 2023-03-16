8eb8052c00b74e2c0089010af9df8bbc1b54b608
Christian de Diego <49655553+christian97dd@users.noreply.github.com>
2023-03-16 14:10:34 -0300

    added try catch to decode token function (#4)
    Co-authored-by: christian97dd <christian.dediego@fizzmod.com>

--- test/useOauth.test.js
+++ test/useOauth.test.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import {renderHook, act} from '@testing-library/react-hooks';
+import * as jwtDecode from 'jwt-decode';
 import * as OAuthUtils from '../src/utils/oauth';
 import * as browserUtils from '../src/utils/browser';
 import * as promisesUtils from '../src/utils/promises';
 import useOauth from '../src/useOauth';
 
-jest.mock('jwt-decode', () =>
-  jest.fn(() => null).mockImplementationOnce(() => ({name: 'example'})),
-);
+// jest.mock('jwt-decode', () =>
+//   jest.fn(() => null).mockImplementationOnce(() => ({name: 'example'})),
+// );
+
+const spyOnJwtDecode = jest.spyOn(jwtDecode, 'default');
 
 describe('useOauth hook', () => {
   afterEach(() => {
@@ -15,29 +18,34 @@ describe('useOauth hook', () => {
   });
 
   test('should initiate with true loading', async () => {
+    spyOnJwtDecode.mockReturnValueOnce(() => ({name: 'example'}));
     const {result} = renderHook(() => useOauth());
     expect(result.current.loading).toBe(true);
   });
 
   test('should update loading to false', async () => {
+    spyOnJwtDecode.mockReturnValueOnce(() => ({name: 'example'}));
     const {result, waitForNextUpdate} = renderHook(() => useOauth());
     await waitForNextUpdate();
     expect(result.current.loading).toBe(false);
   });
 
   test('must call 2 useEffect functions', () => {
+    spyOnJwtDecode.mockReturnValueOnce(() => ({name: 'example'}));
     jest.spyOn(React, 'useEffect');
     renderHook(() => useOauth());
     expect(React.useEffect).toHaveBeenCalledTimes(2);
   });
 
   test('must call getAuthData 1 time', () => {
+    spyOnJwtDecode.mockReturnValueOnce(() => ({name: 'example'}));
     jest.spyOn(OAuthUtils, 'getAuthData');
     renderHook(() => useOauth());
     expect(OAuthUtils.getAuthData).toHaveBeenCalledTimes(1);
   });
 
   test('must call userAuthorize', async () => {
+    spyOnJwtDecode.mockReturnValueOnce(() => ({name: 'example'}));
     jest.spyOn(OAuthUtils, 'userAuthorize');
     const {waitForNextUpdate} = renderHook(() => useOauth());
     await waitForNextUpdate();
@@ -45,6 +53,7 @@ describe('useOauth hook', () => {
   });
 
   test('must call logout method', async () => {
+    spyOnJwtDecode.mockReturnValueOnce(() => ({name: 'example'}));
     jest.spyOn(browserUtils, 'logout');
     const {waitForNextUpdate, result} = renderHook(() => useOauth());
 
@@ -57,6 +66,7 @@ describe('useOauth hook', () => {
   });
 
   test('must setError if something failed in handleLogout', async () => {
+    spyOnJwtDecode.mockReturnValueOnce(() => ({name: 'example'}));
     jest.spyOn(browserUtils, 'logout').mockImplementation(() => {
       throw new Error('Something has failed');
     });
@@ -75,6 +85,7 @@ describe('useOauth hook', () => {
   });
 
   test('should update authData', async () => {
+    spyOnJwtDecode.mockReturnValueOnce(() => ({name: 'example'}));
     jest.spyOn(OAuthUtils, 'getAuthData').mockImplementation(() =>
       Promise.resolve({
         isLogged: true,
@@ -87,6 +98,7 @@ describe('useOauth hook', () => {
   });
 
   test('should update authData', async () => {
+    spyOnJwtDecode.mockReturnValueOnce(() => ({name: 'example'}));
     jest.spyOn(OAuthUtils, 'getAuthData').mockImplementation(() =>
       Promise.resolve({
         isLogged: true,
@@ -96,5 +108,33 @@ describe('useOauth hook', () => {
     const {result, waitForNextUpdate} = renderHook(() => useOauth());
     await waitForNextUpdate();
     expect(result.current.userData).toEqual({});
+  });
+  it('should call decode with fake value in true', async () => {
+    spyOnJwtDecode.mockReturnValueOnce(() => ({name: 'example'}));
+    jest.spyOn(OAuthUtils, 'getAuthData').mockImplementation(() =>
+      Promise.resolve({
+        isLogged: true,
+        oauthTokens: {idToken: 'FAKE.eyJmYWtlIjogdHJ1ZX0='},
+      }),
+    );
+    const {result, waitForNextUpdate} = renderHook(() => useOauth());
+
+    await waitForNextUpdate();
+    console.log('result.current', result.current);
+    expect(result.current.isLogged).toStrictEqual(true);
+  });
+  it('should call decode with fake value in true', async () => {
+    spyOnJwtDecode.mockReturnValueOnce(() => false);
+    jest.spyOn(OAuthUtils, 'getAuthData').mockImplementation(() =>
+      Promise.resolve({
+        isLogged: true,
+        oauthTokens: {idToken: 'eyJmYWtlIjogdHJ1ZX0='},
+      }),
+    );
+    const {result, waitForNextUpdate} = renderHook(() => useOauth());
+
+    await waitForNextUpdate();
+
+    expect(result.current.isLogged).toStrictEqual(true);
   });
 });
