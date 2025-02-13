3fa0e7065a6d1bac86a99b8d78a8dd4ffa24145c
Pablo Nicolás Ortiz <pablonortiz05@hotmail.com>
2025-02-13 14:10:40 -0300

    Added WithTokensExpirationAccess HOC

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ export {getAuthData} from './utils/oauth';
 export * from './useOauthData';
 export * from './utils/getUserInfo';
 export * from './utils/getAccessToken';
+export * from './WithTokensExpirationAccess';
 export {default} from './Provider';
