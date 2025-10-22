a1e0a84c299f05dac79d1e6f3af9de7399c9e930
Gonzalo Lopez <gonzalo.lopez@fizzmod.com>
2025-10-22 15:52:08 -0300

    added export of the logout utility

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [unreleased]
 
+### [v.1.11.0-beta.3] - 2025-10-22
+
+### Added
+
+- export of the logout utility
+
 ### [v.1.11.0-beta.2] - 2025-10-21
 
 ### Changed
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ export {getAuthData} from './utils/oauth';
 export * from './useOauthData';
 export * from './utils/getUserInfo';
 export * from './utils/getAccessToken';
+export * from './utils/browser';
 export * from './withTokensExpirationAccess';
 export {isTokenExpired} from './utils/oauth';
 export {default} from './Provider';
